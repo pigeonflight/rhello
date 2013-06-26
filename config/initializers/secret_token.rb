ebe9a735bdfae785b09aa4b78f23ef14abb55bb9
David Bain <pigeonflight@gmail.com>
2013-06-26 08:11:29 -0400

    updated repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rehello::Application.config.secret_key_base = '0a52c597a8134aade1ed04e511c478af9e6ae39d49221e4f022fdf532a30e42b145aca0d5b12dea2b9d27ed239e3c0f77c65e193f464c267d252bedd401ff375'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rehello::Application.config.session_store :cookie_store, key: '_rehello_session'
